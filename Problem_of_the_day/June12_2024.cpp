f1514bf55db892b684f4dd5eeaf807517bd95f68
Rajar12345 <124551189+Rajar12345@users.noreply.github.com>
2024-06-12 09:16:28 +0530

    Create June12_2024.cpp

---
+++ Problem_of_the_day/June12_2024.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int z=0,o=0,n=nums.size();
+        for(int i=0;i<n;i++){
+            if(nums[i]==0)  z++;
+            else if(nums[i]==1) o++;
+        }
+        for(int i=0;i<z;i++)    nums[i]=0;
+        for(int i=z;i<o+z;i++)    nums[i]=1;
+        for(int i=z+o;i<n;i++)  nums[i]=2;
+    }
+};
