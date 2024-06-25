b75012f14ea2741fb891299da1543a32758a1786
Rajar12345 <124551189+Rajar12345@users.noreply.github.com>
2024-06-25 10:24:49 +0530

    Create June25_2024.cpp

---
+++ Problem_of_the_day/June25_2024.cpp
@@ -0,0 +1,7 @@
+int pre = 0;
+    TreeNode* bstToGst(TreeNode* root) {
+        if (root->right) bstToGst(root->right);
+        pre = root->val = pre + root->val;
+        if (root->left) bstToGst(root->left);
+        return root;
+    }
