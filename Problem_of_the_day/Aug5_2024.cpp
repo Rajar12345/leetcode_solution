772cb378e2da8f2560eb7ccb96cf3eacd2ac0eda
Rajar12345 <124551189+Rajar12345@users.noreply.github.com>
2024-08-05 12:24:08 +0530

    Update Aug5_2024.cpp

--- Problem_of_the_day/Aug5_2024.cpp
+++ Problem_of_the_day/Aug5_2024.cpp
@@ -1,27 +1,16 @@
-if(!root) return {};
-        else if(!root->left && !root->right) return {root->data};
-        map<int, int>mpp;
-        queue<pair<Node*, int>>q;
-        q.push({root, 0});
-        
-        while(!q.empty()){
-            auto it=q.front();
-            q.pop();
-            
-            Node* node=it.first;
-            int line=it.second;
-            mpp[line]=node->data;
-            
-            if(node->left){
-                q.push({node->left, line-1});
-            }
-            if(node->right){
-                q.push({node->right, line+1});
+class Solution {
+public:
+    string kthDistinct(vector<string>& arr, int k) {
+        unordered_map<string, int> mp;
+        for (string& s : arr)
+            mp[s]++; 
+        for (string& s : arr) { 
+            if (mp[s] == 1) {
+                k--;          
+                if (k == 0)
+                    return s; 
             }
         }
-        vector<int>ans;
-        
-        for(auto &it:mpp){
-            ans.push_back(it.second);
-        }
-        return ans;
+        return "";
+    }
+};
